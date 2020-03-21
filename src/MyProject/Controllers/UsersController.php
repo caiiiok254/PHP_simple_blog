31885cca26bada373c3003afc98fb84216b03556
caiiiok254 <kiskamiska2000@gmail.com>
2020-03-21 23:00:57 +0700

    Add registration system

---
+++ src/MyProject/Controllers/UsersController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Myproject\Controllers;
+
+use MyProject\Exceptions\InvalidArgumentException;
+use Myproject\View\View;
+use MyProject\Models\Users\User;
+
+class UsersController
+{
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View(__DIR__ . "/../../templates");
+    }
+
+    public function signUp()
+    {
+        if (!empty($_POST)) {
+            try {
+                $user = User::signUp($_POST);
+            } catch (InvalidArgumentException $e) {
+                $this->view->renderHtml("users/signUp.php", ["error" => $e->getMessage()]);
+                return;
+            }
+
+            if ($user instanceof User) {
+                $this->view->renderHtml("users/signUpSuccessful.php");
+                return;
+            }
+        }
+
+        $this->view->renderHtml("users/signUp.php");
+    }
+}
+
+
--- src/MyProject/Models/Users/User.php
+++ src/MyProject/Models/Users/User.php
@@ -2,7 +2,8 @@
 
 namespace MyProject\Models\Users;
 
-use Myproject\Models\ActiveRecordEntity;
+use MyProject\Exceptions\InvalidArgumentException;
+use MyProject\Models\ActiveRecordEntity;
 
 class User extends ActiveRecordEntity
 {
@@ -13,6 +14,52 @@ class User extends ActiveRecordEntity
     protected $passwordHash;
     protected $authToken;
     protected $createdAt;
+
+    public static function signUp(array $userData): User
+    {
+        if (empty($userData["nickname"])) {
+            throw new InvalidArgumentException("Не передан nickname");
+        }
+
+        if (!preg_match("/^[a-zA-Z0-9]+$/", $userData["nickname"])) {
+            throw new InvalidArgumentException("Nickname может состоять только из символов латинского алфавита и цифр");
+        }
+
+        if (empty($userData["email"])) {
+            throw new InvalidArgumentException("Не передан email");
+        }
+
+        if (!filter_var($userData["email"], FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidArgumentException("Email некорректен");
+        }
+
+        if (empty($userData["password"])) {
+            throw new InvalidArgumentException("Не передан password");
+        }
+
+        if (mb_strlen($userData["password"]) < 8) {
+            throw new InvalidArgumentException("Пароль должен содержать не менее 8 символов");
+        }
+
+        if (static::findOneByColumn("nickname", $userData["nickname"]) !== null) {
+            throw new InvalidArgumentException("Пользователь с таким nickname уже существует");
+        }
+
+        if (static::findOneByColumn("email", $userData["email"]) !== null) {
+            throw new InvalidArgumentException("Пользователь с таким email уже существует");
+        }
+
+        $user = new User();
+        $user->nickname = $userData["nickname"];
+        $user->email = $userData["email"];
+        $user->passwordHash = password_hash($userData["password"], PASSWORD_DEFAULT);
+        $user->isConfirmed = false;
+        $user->role = "user";
+        $user->authToken = sha1(random_bytes(100)) . sha1(random_bytes(100));
+        $user->save();
+
+        return $user;
+    }
 
     public function getEmail(): string
     {
